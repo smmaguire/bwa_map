3aa17e12e50c15efc89aad83ef3cc99c9e6f9d96
Sean Maguire <smmaguire@gmail.com>
2020-09-21 16:07:58 -0400

    fix ss

--- bwa_meth_pipe_ss.sh
+++ bwa_meth_pipe_ss.sh
@@ -14,7 +14,8 @@ fastqc ${r2} --outdir=${out}"/qc_pretrim/"
 ### Trim reads
 ### Trim off the nicking site of read2. Then trim the adapters.
 adap_path=/mnt/home/smaguire/work/rloops/adapter_files/adapter1.fasta 
-flexbar -r ${r2} --pre-trim-left 5 --stdout-reads | flexbar -r ${r1} -p - -a $adap_path -ap ON -q TAIL -qf i1.8 -qt 25 -t ${out}"/trim_data/"${name}
+flexbar -r ${r2} --pre-trim-left 5 -t ${out}"/trim_data_temp/"${name}"r2_trim"
+flexbar -r ${r1} -p ${out}"/trim_data_temp/"${name}"r2_trim" -a $adap_path -ap ON -q TAIL -qf i1.8 -qt 25 -t ${out}"/trim_data/"${name}
 
 ### fastqc2
 fastqc ${out}"/trim_data/"${name}_1.fastq --outdir=${out}"/qc_posttrim/"
