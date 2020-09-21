bb04ee500fba111d311fa7ebc1f49706747e1de8
Sean Maguire <smmaguire@gmail.com>
2020-09-21 14:33:27 -0400

    Update bwa_meth_pipe.sh

--- bwa_meth_pipe.sh
+++ bwa_meth_pipe.sh
@@ -9,5 +9,6 @@
 source activate bwa_meth
 ### fastqc1
 fastqc ${r1} --outdir=${out}"/qc_pretrim/"
+fastqc ${r2} --outdir=${out}"/qc_pretrim/"
 
-flexbar -r ${r1} -p ${r2} -aa nextara -ap ON -t ${out}"/trim_data/"$new_name.fastq
+flexbar -r ${r1} -p ${r2} -aa TruSeq -ap ON -t ${out}"/trim_data/"$new_name.fastq
