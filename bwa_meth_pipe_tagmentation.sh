782bc96be69b7b1edd4f42f1dfab08a8e3eda173
Sean Maguire <smmaguire@gmail.com>
2020-10-07 11:01:30 -0400

    update to run with tagment2 data

---
+++ bwa_meth_pipe_tagmentation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -pe smp 1
+#$ -m e
+
+source activate bwa_meth
+### fastqc1
+fastqc ${r1} --outdir=${out}"/qc_pretrim/"
+fastqc ${r2} --outdir=${out}"/qc_pretrim/"
+
+### Trim reads
+flexbar -r ${r1} -p ${r2} --adapter-preset Nextera -ap ON -q TAIL -qf i1.8 -qt 25 -t ${out}"/trim_data/"${name}
+
+### fastqc2
+fastqc ${out}"/trim_data/"${name}_1.fastq --outdir=${out}"/qc_posttrim/"
+fastqc ${out}"/trim_data/"${name}_2.fastq --outdir=${out}"/qc_posttrim/"
+
+#### map with bwa_meth
+bwameth.py --reference /mnt/home/smaguire/work/rloops/reference_files/ptw120_ref.fasta ${out}"/trim_data/"${name}_1.fastq ${out}"/trim_data/"${name}_2.fastq | \
+samtools view -b - | samtools sort - > ${out}"/mapped_data/"${name}".bam"
+samtools index ${out}"/mapped_data/"${name}".bam"
+
+#### extract methylation amounts with methyldackel
+#### MethylDackel extract /mnt/home/smaguire/work/rloops/reference_files/m13_ref.fasta ${out}"/mapped_data/"${name}".bam"
+
--- launch_bwa_meth.sh
+++ launch_bwa_meth.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-data_path=/mnt/home/smaguire/work/rloops/data/apobec_concentration
-mkdir -p /mnt/home/smaguire/work/rloops/apobec_concentration_output
-output_path=/mnt/home/smaguire/work/rloops/apobec_concentration_output
+data_path=/mnt/home/smaguire/work/rloops/data/whole_genome_method/tagment_plasmid2
+mkdir -p /mnt/home/smaguire/work/rloops/whole_genome_method_output/tagment_plasmid2
+output_path=/mnt/home/smaguire/work/rloops/whole_genome_method_output/tagment_plasmid2
 mkdir -p $output_path"/qc_pretrim"
 mkdir -p $output_path"/qc_posttrim"
 mkdir -p $output_path"/trim_data"
 mkdir -p $output_path"/mapped_data"
 
-for i in $( ls $data_path/dsDNA*_L001_R1_001.fastq.gz);
+for i in $( ls $data_path/*.1.fastq.gz);
 do
 new_name=${i#$data_path/}
-new_name=${new_name%_L001_R1_001.fastq.gz}
+new_name=${new_name%.1.fastq.gz}
 echo $new_name 
-qsub -v r1=$data_path/$new_name"_L001_R1_001.fastq.gz",r2=$data_path/$new_name"_L001_R2_001.fastq.gz",out=$output_path,name=$new_name bwa_meth_pipe_ds.sh
+qsub -v r1=$data_path/$new_name".1.fastq.gz",r2=$data_path/$new_name".2.fastq.gz",out=$output_path,name=$new_name bwa_meth_pipe_ds.sh
 done
 
 
-for i in $( ls $data_path/ssDNA*_L001_R1_001.fastq.gz);
-do
-new_name=${i#$data_path/}
-new_name=${new_name%_L001_R1_001.fastq.gz}
-echo $new_name 
-qsub -v r1=$data_path/$new_name"_L001_R1_001.fastq.gz",r2=$data_path/$new_name"_L001_R2_001.fastq.gz",out=$output_path,name=$new_name bwa_meth_pipe_ss.sh
-done
+##for i in $( ls $data_path/ssDNA*_L001_R1_001.fastq.gz);
+##do
+##new_name=${i#$data_path/}
+##new_name=${new_name%_L001_R1_001.fastq.gz}
+##echo $new_name 
+##qsub -v r1=$data_path/$new_name"_L001_R1_001.fastq.gz",r2=$data_path/$new_name"_L001_R2_001.fastq.gz",out=$output_path,name=$new_name bwa_meth_pipe_ss.sh
+##done
