56cfba7f686b1aa205aa85cd42625dfd220b7825
Sean Maguire <smmaguire@gmail.com>
2020-10-08 07:50:26 -0400

    update for apocon3

--- bwa_meth_pipe.sh
+++ bwa_meth_pipe.sh
@@ -12,9 +12,17 @@ fastqc ${r1} --outdir=${out}"/qc_pretrim/"
 fastqc ${r2} --outdir=${out}"/qc_pretrim/"
 
 ### Trim reads
-flexbar -r ${r1} -p ${r2} -aa TruSeq -ap ON -t ${out}"/trim_data/"${name}
+flexbar -r ${r1} -p ${r2} --adapter-preset TruSeq -ap ON -q TAIL -qf i1.8 -qt 25 -t ${out}"/trim_data/"${name}
 
 ### fastqc2
 fastqc ${out}"/trim_data/"${name}_1.fastq --outdir=${out}"/qc_posttrim/"
 fastqc ${out}"/trim_data/"${name}_2.fastq --outdir=${out}"/qc_posttrim/"
+
+#### map with bwa_meth
+bwameth.py --reference $reference ${out}"/trim_data/"${name}_1.fastq ${out}"/trim_data/"${name}_2.fastq | \
+samtools view -b - | samtools sort - > ${out}"/mapped_data/"${name}".bam"
+samtools index ${out}"/mapped_data/"${name}".bam"
+
+#### extract methylation amounts with methyldackel
+#### MethylDackel extract /mnt/home/smaguire/work/rloops/reference_files/m13_ref.fasta ${out}"/mapped_data/"${name}".bam"
 
--- launch_bwa_meth.sh
+++ launch_bwa_meth.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-data_path=/mnt/home/smaguire/work/rloops/data/whole_genome_method/tagment_plasmid2
-mkdir -p /mnt/home/smaguire/work/rloops/whole_genome_method_output/tagment_plasmid2
-output_path=/mnt/home/smaguire/work/rloops/whole_genome_method_output/tagment_plasmid2
+data_path=/mnt/home/smaguire/work/rloops/data/apobec_concentration/apo_con3
+output_path=/mnt/home/smaguire/work/rloops/apobec_concentration_output/apo_con3
+reference_path=/mnt/home/smaguire/work/rloops/reference_files/lambda.fasta
+mkdir -p $output_path
 mkdir -p $output_path"/qc_pretrim"
 mkdir -p $output_path"/qc_posttrim"
 mkdir -p $output_path"/trim_data"
@@ -13,7 +14,7 @@ do
 new_name=${i#$data_path/}
 new_name=${new_name%.1.fastq.gz}
 echo $new_name 
-qsub -v r1=$data_path/$new_name".1.fastq.gz",r2=$data_path/$new_name".2.fastq.gz",out=$output_path,name=$new_name bwa_meth_pipe_tagmentation.sh
+qsub -v r1=$data_path/$new_name".1.fastq.gz",r2=$data_path/$new_name".2.fastq.gz",out=$output_path,name=$new_name,reference=$reference_path bwa_meth_pipe.sh
 done
 
 
