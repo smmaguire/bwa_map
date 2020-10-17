8d09d10f785a7b6594007fd1e77452376b729131
Sean Maguire <smmaguire@gmail.com>
2020-10-17 17:00:07 -0400

    needs nextrea adapters

--- bwa_meth_pipe.sh
+++ bwa_meth_pipe.sh
@@ -12,7 +12,8 @@ fastqc ${r1} --outdir=${out}"/qc_pretrim/"
 fastqc ${r2} --outdir=${out}"/qc_pretrim/"
 
 ### Trim reads
-flexbar -r ${r1} -p ${r2} --adapter-preset TruSeq -ap ON -q TAIL -qf i1.8 -qt 25 -t ${out}"/trim_data/"${name}
+#flexbar -r ${r1} -p ${r2} --adapter-preset TruSeq -ap ON -q TAIL -qf i1.8 -qt 25 -t ${out}"/trim_data/"${name}
+flexbar -r ${r1} -p ${r2} --adapter-preset Nextera -ap ON -q TAIL -qf i1.8 -qt 25 -t ${out}"/trim_data/"${name}
 
 ### fastqc2
 fastqc ${out}"/trim_data/"${name}_1.fastq --outdir=${out}"/qc_posttrim/"
